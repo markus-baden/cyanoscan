1207dd0de396808cbdfe8471609cef44278f2aa4
Markus Baden <markus-baden@gmx.de>
2023-02-07 11:53:04 +0100

    download sat images with water check

--- data-collection/data_collection.py
+++ data-collection/data_collection.py
@@ -141,15 +141,45 @@ def normalize_sentinel(arr):
     
     return arr_new#.astype(int)
 
+def get_ndwi_mask(image_layers, ndwi_limit=0.12):
+    """Calculates image mask based on NDWI
+    NDWI - Normalized Difference Water Index
+    NDWI = (green - NIR) / (green + NIR)
+    source: https://custom-scripts.sentinel-hub.com/custom-scripts/sentinel-2/ndwi/
+    another source: https://en.wikipedia.org/wiki/Normalized_difference_water_index
 
+    Args:
+        image_layers (_type_): Satellite image layers
+        ndwi_limit (float, optional): Limit value for masking. According to sources 0.3 is standard, 
+            but achieved better results with defaulting to 0.12.
 
-def collect_sat_image_layers(row, sat_image_catalog, image_size=128, verbose=False):
+    Returns:
+        ndwi_mask: True/False mask, True where water is found
+        ndwi_image: 1-layer image of the NDWI
+    """
+    # Calculate NDWI layers: (green - NIR) / (green + NIR)
+    a = (image_layers['green'] - image_layers['nir']) 
+    b = (image_layers['green'] + image_layers['nir'])
+    # this line is safe against division by 0 errors:
+    ndwi_image = np.array([np.divide(a, b, out=np.zeros(a.shape, dtype=float), where=b!=0)]) 
+    
+    # create mask based on NDWI values
+    ndwi_mask = (ndwi_image[0] > ndwi_limit)
+    return ndwi_mask, ndwi_image
+
+def collect_sat_image_layers(row, 
+                            sat_image_catalog, 
+                            image_size=128, 
+                            verbose=False, 
+                            check_for_water=True, 
+                            limit=0.12, 
+                            days=15):
     # Search in catalog based on bounding box and date
     # calculate rough estimate of the bounding box size based on Sentinel resolution 10m/pixel
     meter_buffer = ((10 * image_size) / 2) - 5
     bbox = get_bounding_box(row.latitude, row.longitude, meter_buffer=meter_buffer)
 
-    date_range = get_date_range(row.date, time_buffer_days=15)
+    date_range = get_date_range(row.date, time_buffer_days=days)
 
     # search the planetary computer sentinel-l2a and landsat level-2 collections
     search_results = sat_image_catalog.search(
@@ -184,91 +214,80 @@ def collect_sat_image_layers(row, sat_image_catalog, image_size=128, verbose=Fal
     # check how many Sentinel and Landsat images are available
     num_sentinel = len(item_details[item_details.platform.str.contains("Sentinel")])
     num_landsat  = len(item_details[item_details.platform.str.contains("landsat")])
+    items_sentinel = item_details[item_details.platform.str.contains("Sentinel")].sort_values(by="datetime", ascending=False)
+    items_landsat  = item_details[item_details.platform.str.contains("landsat")].sort_values(by="datetime", ascending=False)
     if verbose:
         print(f"Datapoint ID: {row.uid}")
         print(f"Number of Sentinel items found: {num_sentinel}")
         print(f"Number of Landsat items found: {num_landsat}")
 
     # collect layers from catalog (based on Sentinel or Landsat or none)
-    image_layers = dict.fromkeys(BAND_NAMES, np.array(0))
     if num_sentinel > 0:
-        # If sentinel is available, it is first choice.
-        if verbose: 
-            print('Sentinel')
-        # Select closest image by date.
-        # TODO: Refine selection by using the less cloudy image when there's more than one image for the date??
-        item = (
-            item_details[item_details.platform.str.contains("Sentinel")]
-            .sort_values(by="datetime", ascending=False)
-            .iloc[0]
-            ).item_obj
-        # collect layers in dict
-        for band, name in zip(SENTINEL_BANDS, BAND_NAMES):
-            image = rioxarray.open_rasterio(pc.sign(item.assets[band].href)).rio.clip_box(
-                minx=bbox[0],
-                miny=bbox[1],
-                maxx=bbox[2],
-                maxy=bbox[3],
-                crs="EPSG:4326",
-            ).to_numpy()
-            # normalize pixel values into range 0.0 to 1.0
-            normalized = normalize_sentinel(image[0])
-            # resize image if it does not match "image_size"
-            if (normalized.shape[0] != image_size) or (normalized.shape[1] != image_size):
-                #print(f'Shape {name}: {normalized.shape} -->RESIZED!!!')
-                image_layers[name] = cv2.resize(normalized, ( image_size, image_size), interpolation = cv2.INTER_AREA)
+        for item in items_sentinel.item_obj:
+            # If sentinel is available, it is first choice.
+            if verbose: 
+                print('Searching through Sentinel items')
+            # Select closest image by date.
+            # TODO: Refine selection by using the less cloudy image when there's more than one image for the date??
+            #item_object = item.item_obj
+            # collect layers in dict
+            image_layers = dict.fromkeys(BAND_NAMES, np.array(0))
+            for band, name in zip(SENTINEL_BANDS, BAND_NAMES):
+                image = rioxarray.open_rasterio(pc.sign(item.assets[band].href)).rio.clip_box(
+                    minx=bbox[0],
+                    miny=bbox[1],
+                    maxx=bbox[2],
+                    maxy=bbox[3],
+                    crs="EPSG:4326",
+                ).to_numpy()
+                # normalize pixel values into range 0.0 to 1.0
+                normalized = normalize_sentinel(image[0])
+                # resize image if it does not match "image_size"
+                if (normalized.shape[0] != image_size) or (normalized.shape[1] != image_size):
+                    #print(f'Shape {name}: {normalized.shape} -->RESIZED!!!')
+                    image_layers[name] = cv2.resize(normalized, ( image_size, image_size), interpolation = cv2.INTER_AREA)
+                else:
+                    image_layers[name] = normalized
+
+            if check_for_water:
+                # check if water is visible (NDWI mask must have at least 1 True entry)
+                mask, _ = get_ndwi_mask(image_layers, ndwi_limit=limit)
+                # if water is found stop searching
+                if mask.sum():
+                    return image_layers, 'sentinel'
             else:
-                image_layers[name] = normalized
+                # return first found image directly (which is the latest by date)
+                return image_layers, 'sentinel'
 
-    elif num_landsat > 0:
-        # If only Landsat is available we use it instead
-        if verbose:
-            print('Landsat')
-        item = (
-            item_details[item_details.platform.str.contains("landsat")]
-            .sort_values(by="datetime", ascending=False)
-            .iloc[0]
-            ).item_obj
-        # collect layers in dict
-        for band, name in zip(LANDSAT_BANDS, BAND_NAMES):
-            image = odc.stac.stac_load([pc.sign(item)], bands=[band], bbox=bbox).isel(time=0)
-            # scale pixel values into practical range
-            normalized = normalize_landsat(image[[band]].to_array().to_numpy()[0])
-            # upscale landsat images to same pixel size as sentinel
-            image_layers[name] = cv2.resize(normalized, ( image_size, image_size), interpolation = cv2.INTER_AREA)
-    else:
-        # use array of zeros when no image is found
-        if verbose:
-            print("No Image was found!")
-        for band, name in zip(LANDSAT_BANDS, BAND_NAMES):
-            image_layers[name] = np.zeros([image_size, image_size])
-    
-    return image_layers
-    
+    if num_landsat > 0:
+        for item in items_landsat.item_obj:
+            if verbose: 
+                print('Searching through Landsat items')
+            # If only Landsat is available we use it instead
+            # collect layers in dict
+            image_layers = dict.fromkeys(BAND_NAMES, np.array(0))
+            for band, name in zip(LANDSAT_BANDS, BAND_NAMES):
+                image = odc.stac.stac_load([pc.sign(item)], bands=[band], bbox=bbox).isel(time=0)
+                # scale pixel values into practical range
+                normalized = normalize_landsat(image[[band]].to_array().to_numpy()[0])
+                # upscale landsat images to same pixel size as sentinel
+                image_layers[name] = cv2.resize(normalized, ( image_size, image_size), interpolation = cv2.INTER_AREA)
 
+            if check_for_water:
+                # check if water is visible (NDWI mask must have at least 1 True entry)
+                mask, _ = get_ndwi_mask(image_layers, ndwi_limit=limit)
+                # if water is found stop searching
+                if mask.sum():
+                    return image_layers, 'landsat'
+            else:
+                # return first found image directly (which is the latest by date)
+                return image_layers, 'landsat'
 
-def get_ndwi_mask(image_layers, ndwi_limit=0.12):
-    """Calculates image mask based on NDWI
-    NDWI - Normalized Difference Water Index
-    NDWI = (green - NIR) / (green + NIR)
-    source: https://custom-scripts.sentinel-hub.com/custom-scripts/sentinel-2/ndwi/
-    another source: https://en.wikipedia.org/wiki/Normalized_difference_water_index
+    # if no images are found use array of zeros when no image is found
+    image_layers = dict.fromkeys(BAND_NAMES, np.array(0))
+    if verbose:
+        print("No Image was found!")
+    for band, name in zip(LANDSAT_BANDS, BAND_NAMES):
+        image_layers[name] = np.zeros([image_size, image_size])
 
-    Args:
-        image_layers (_type_): Satellite image layers
-        ndwi_limit (float, optional): Limit value for masking. According to sources 0.3 is standard, 
-            but achieved better results with defaulting to 0.12.
-
-    Returns:
-        ndwi_mask: True/False mask, True where water is found
-        ndwi_image: 1-layer image of the NDWI
-    """
-    # Calculate NDWI layers: (green - NIR) / (green + NIR)
-    a = (image_layers['green'] - image_layers['nir']) 
-    b = (image_layers['green'] + image_layers['nir'])
-    # this line is safe against division by 0 errors:
-    ndwi_image = np.array([np.divide(a, b, out=np.zeros(a.shape, dtype=float), where=b!=0)]) 
-    
-    # create mask based on NDWI values
-    ndwi_mask = (ndwi_image[0] > ndwi_limit)
-    return ndwi_mask, ndwi_image
+    return image_layers, 'no_image'
